914ce5a79579c1f651269162f14fbde1119bb990
Lethabo Phofa <lethabophofa@gmail.com>
2023-02-05 13:54:50 +0200

    hello-world

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace x4amcd
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            String name = "x4amcd";
+            Console.WriteLine($"Hello, World! My name is {name}.");
+            Console.ReadKey(true);
+            using (var stream = Console.OpenStandardInput())
+            {
+                var buffer = new byte[1];
+                var bytesRead = await stream.ReadAsync(buffer, 0, 1);
+                char c = (char)buffer[0];
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
